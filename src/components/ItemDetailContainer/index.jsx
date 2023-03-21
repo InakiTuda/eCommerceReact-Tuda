a767f948476d344fa0914152b86528e399272db8
Iñaki Tuda <inakituda@gmail.com>
2023-03-21 12:13:18 -0300

    error array

--- src/components/ItemDetailContainer/index.jsx
+++ src/components/ItemDetailContainer/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useEffect, useState, } from "react";
-import { useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import ItemDetail from "./ItemDetail";
 import arrayProductos from "./json/arrayProductos.json";
 
