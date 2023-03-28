accbf0573acb8c5ae54fd66f75f37eb28b5b2a75
Iñaki Tuda <inakituda@gmail.com>
2023-03-28 19:37:42 -0300

    cambie css

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 const Item = ({item}) => {
     return (
             <div className="card">
-                <div className="card border border-0">
+                <div className="card border border-0 ">
                     <img src={item.imagen} className="card-img-top" alt={item.nombre} />
                     <div className="card-body text-center">
                         <b className="card-text text-black text-uppercase">{item.nombre}</b>
                         <p className="card-price text-black">${item.precio}</p>
                     </div>
                 </div>
-                <Link to={`/item/${item.id}`} className="card border border-0 text-decoration-none">
+                <Link to={`/item/${item.id}`} className="card border text-decoration-none">
                 <button>Ver Detalle</button>
                 </Link>
             </div>
